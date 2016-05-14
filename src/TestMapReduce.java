1187f391d7c109161b13ebe497735476e18db5d3
amangrover <manigroveris@gmail.com>
2016-05-14 23:06:25 +0530

    for checking unique words

--- src/TestMapReduce.java
+++ src/TestMapReduce.java
@@ -12,26 +12,37 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class TestMapReduce {
     //test comment
-    private static class PhraseBidMapper extends Mapper<LongWritable, Text, LongWritable, DoubleWritable> {
+    private static class PhraseBidMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
         private LongWritable phraseIDKey = new LongWritable();
+        private Text t = new Text();
         private DoubleWritable bidValue = new DoubleWritable();
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            int size,day,bid,impression,click,rank;
             String line = value.toString();
             String[] splitLine = line.split("\t");
-            phraseIDKey.set(Long.parseLong(splitLine[1]));
-            bidValue.set(Double.parseDouble(splitLine[3]));
-            context.write(phraseIDKey, bidValue);
+            size = splitLine.length;
+            //int[] keyphrase = new int[size-6];
+            bid = Integer.parseInt(splitLine[4]);
+            impression = Integer.parseInt(splitLine[5]);
+            click = Integer.parseInt(splitLine[6]);
+            rank = Integer.parseInt(splitLine[2]);
+            for(int i = 0; i<size-6;i++){
+                t.set(splitLine[i+2]);
+                bidValue.set(1);
+                context.write(t,bidValue);
+            }
+
+//            phraseIDKey.set(Long.parseLong(splitLine[1]));
+//            bidValue.set(Double.parseDouble(splitLine[3]));
+//            context.write(phraseIDKey, bidValue);
         }
     }
 
-    private static class DoubleMaxReducer extends Reducer<LongWritable, DoubleWritable, LongWritable, DoubleWritable> {
+    private static class DoubleMaxReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
         private DoubleWritable maxBidValue = new DoubleWritable();
-        public void reduce(LongWritable key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException{
+        public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException{
             Double max = 0.0;
-            for(DoubleWritable val: values){
-                max = Math.max(max, val.get());
-            }
-            maxBidValue.set(max);
+            maxBidValue.set(0);
             context.write(key, maxBidValue);
         }
     }
